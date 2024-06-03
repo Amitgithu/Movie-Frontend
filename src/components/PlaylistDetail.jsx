adc8c524ea9fa5d9dd987e10aae74314c6d760fe
Amitgithu <amit909563@gmail.com>
2024-06-03 23:40:45 +0530

    commit

---
+++ src/components/PlaylistDetail.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams, Link } from 'react-router-dom';
+import Navbar from './Navbar';
+
+function PlaylistDetail() {
+  const { playlist_id } = useParams();
+  const [playlist, setPlaylist] = useState(null);
+
+  useEffect(() => {
+    const fetchPlaylist = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8000/playlists/${playlist_id}`);
+        setPlaylist(res.data.movieDetails);
+      } catch (error) {
+        console.error('Error fetching playlist:', error);
+      }
+    };
+
+    fetchPlaylist();
+  }, [playlist_id]);
+
+  return (
+    <div>
+      <Navbar />
+      {playlist != null ? (
+        <div className="container mt-4">
+          <h3 className="text-center text-secondary mb-4">Movies Inside Playlist</h3>
+          <div className="row">
+            {playlist.map((movie) => (
+              <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-4" key={movie.imdbID}>
+                <Link to={`/movieDetail/${movie.imdbID}`}>
+                  <div className="card h-100">
+                    <img
+                      src={movie.Poster}
+                      className="card-img-top"
+                      alt={movie.Title}
+                      style={{ height: '250px', objectFit: 'cover' }}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        {movie.Title.length > 15
+                          ? `${movie.Title.substring(0, 15)}...`
+                          : movie.Title}
+                      </h5>
+                      <p className="card-text">{movie.Year}</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <h3 className="text-center text-secondary mt-4">Loading...</h3>
+      )}
+    </div>
+  );
+}
+
+export default PlaylistDetail;
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./Navbar";
+
+function SignUp() {
+  let [name, setName] = useState("");
+  let [password, setPassword] = useState("");
+  let [email, setEmail] = useState("");
+
+  let navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:8000/register", {
+        name,
+        email,
+        password,
+      });
+      let userInfo = response.data;
+      if (userInfo.isUserExist) {
+        toast.warning("User Already Exist");
+        navigate("/login");
+      } else {
+        toast.success("Register Successfully!");
+        navigate("/login");
+      }
+    } catch (err) {
+      console.log("Something went wrong: ", err);
+    }
+  }
+
+  return (
+    <>
+      <Navbar />
+      <form onSubmit={handleSubmit} className="container py-5  d-flex align-items-center justify-content-center">
+        <div className="col-12 col-md-8 col-lg-6 col-xl-5 p-4 bg-white shadow rounded">
+          <h3 className="mb-5 text-center">CREATE AN ACCOUNT</h3>
+
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              name="UserName"
+              placeholder="UserName"
+            />
+            <label htmlFor="floatingPassword">Name</label>
+          </div>
+
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+              placeholder="Email"
+            />
+            <label htmlFor="floatingPassword">Enter Your Email</label>
+          </div>
+
+          <div className="form-floating mb-3">
+            <input
+              type="password"
+              className="form-control"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+              placeholder="Password"
+            />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
+
+          <div className="d-grid">
+            <button className="btn btn-primary btn-block" type="submit">
+              Register
+            </button>
+          </div>
+
+          <h6 className="text-center mt-5">
+            Have already an account?{" "}
+            <Link to={"/login"} style={{ color: "#0d6efd" }}>
+              Login here
+            </Link>
+          </h6>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default SignUp;
